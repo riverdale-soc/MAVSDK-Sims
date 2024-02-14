e7da79639a971dc3b0444b006c10ab5a7e3fac94
DmitriLyalikov <Dlyalikov01@manhattan.edu>
2024-02-14 18:33:44 -0500

    update go to submod location

--- GoTo_Submersion_Module_Location/main.py
+++ GoTo_Submersion_Module_Location/main.py
@@ -7,106 +7,77 @@ Careful, we are using python 2.7 here.
 
 """
 
-from __future__ import print_function
-import time
-from dronekit import connect, VehicleMode, LocationGlobalRelative
+from mavsdk import System
+import asyncio
 
-import argparse
-parser = argparse.ArgumentParser(description='Commands vehicle using vehicle.simple_goto.')
-parser.add_argument('--connect', 
-                     help="Vehicle connection target string. If not specified, SITL automatically started and used.")
 
-args = parser.parse_args()
-connection_string = args.connect
-sitl = None 
+async def run():
+    # Start a connection to the vehicle
+    vehicle = System()
+    await vehicle.connect(system_address="udp://:14540")
 
-# Start SITL if no connection string specified
-if not connection_string:
-    import dronekit_sitl
-    sitl = dronekit_sitl.start_default()
-    connection_string = sitl.connection_string()
+    async def arm_and_takeoff(target_altitude):
+        print("Basic pre-arm checks")
+        # Don't let the user try to arm until autopilot is ready
 
-# Connect to the Vehicle
-print('Connecting to vehicle on: %s' % connection_string)
-vehicle = connect(connection_string, wait_ready=True)
+        async def wait_for_armable():
+            while not await vehicle.telemetry.health_all_ok():
+                print("Waiting for system to be ready")
+                await asyncio.sleep(1)
 
-def arm_and_takeoff(aTargetAltitude):
-    """
-    Arms vehicle and fly to aTargetAltitude.
-    """
-    print("Basic pre-arm checks")
+        await wait_for_armable()
 
-    # Don't try to arm until autopilot is ready
-    while not vehicle.is_armable:
-        print(" Waiting for vehicle to initialise...")
-        time.sleep(1)
+        print("Arming motors")
+        # Copter should arm in GUIDED mode
+        await vehicle.action.set_takeoff_altitude(target_altitude)
+        await vehicle.action.arm()
+        await vehicle.action.takeoff()
 
-    print("Arming motors")
-    # Copter should arm in GUIDED mode
-    vehicle.mode = VehicleMode("GUIDED")
-    vehicle.armed = True
+        # Confirm vehicle armed before attempting to take off
+        while not await vehicle.telemetry.armed():
+            print(" Waiting for arming...")
+            await asyncio.sleep(1)
 
-    # Confirm vehicle armed before attempting to take off
-    while not vehicle.armed:
-        print(" Waiting for arming...")
-        time.sleep(1)
+        print("Taking off!")
 
-    print("Taking off!")
-    # Take off and fly the vehicle to a specified altitude (meters)
-    vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
+        # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
+        async def wait_until_altitude_reached():
+            while (await vehicle.telemetry.altitude()).relative_altitude_m < target_altitude * 0.95:
+                print(" Altitude: ", (await vehicle.telemetry.altitude()).relative_altitude_m)
+                await asyncio.sleep(1)
 
-    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
-    #  after Vehicle.simple_takeoff will execute immediately).
-    while True:
-        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
-        #Break and return from function just below target altitude.        
-        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95: 
-            print("Reached target altitude")
-            break
-        time.sleep(1)
+        await wait_until_altitude_reached()
 
-arm_and_takeoff(10)
+    await arm_and_takeoff(10)
 
-print("Set default/target airspeed to 3")
-# Airspeed in meters/second
-vehicle.airspeed = 3
+    print("Set default/target airspeed to 3")
+    # Airspeed in m/s
+    await vehicle.action.set_return_to_launch_altitude(10)
+    await vehicle.action.set_maximum_speed(3)
 
-print("Going towards first point for 30 seconds ...")
-point1 = LocationGlobalRelative(47.397742, 8.545594, 10)
-vehicle.simple_goto(point1)
+    print("Going towards first point for 30 seconds ...")
+    point1 = [47.398005, 8.545738, 10]
+    await vehicle.action.goto_location(*point1)
 
-# sleep so we can see the change in map
-time.sleep(30)
+    # sleep so we can see the change in map
+    await asyncio.sleep(30)
 
-print("Going towards second point for 30 seconds (groundspeed set to 10 m/s) ...")
-# Global location object: altitude relative to home location altitude
-# Relative to WGS84, latitude, longitude, and altitude in meters
-point2 = LocationGlobalRelative(47.397742, 8.545594, 10)
+    print("Going towards second point for 30 seconds (groundspeed set to 10 m/s) ...")
+    point2 = [47.390331, 8.564214, 10]
+    await vehicle.action.goto_location(*point2)
 
-vehicle.simple_goto(point2, groundspeed=10)
+    # sleep so we can see the change in map
+    await asyncio.sleep(30)
 
-# sleep so we can see the change in map
-time.sleep(30)
+    print("Returning to Launch")
+    await vehicle.action.return_to_launch()
 
-print("Returning to Launch")  
-"""
-RTL Mode navigates the vehicle from its current position to hover above the home position.
-The behavior of the mode is determined by the vehicle's home_location and the RTL_ALT parameter.
+    print("Close vehicle object")
+    await vehicle.close()
 
-THe copter will rise a minimum of RLT_CLIMB_MIN meters or to RTL_ALT, whichever is higher, before returning home.
-The default value for for RTL_ALT is 15 meters.
+if __name__ == "__main__":
+    asyncio.run(run())
 
-This means if you execute an RTL in Copter, it will return to the location where it was armed.
-""" 
-vehicle.mode = VehicleMode("RTL")
 
-# Close vehicle object before exiting script
-print("Close vehicle object")
-vehicle.close()
 
-# Shut down simulator
-if sitl:
-    sitl.stop()
-
-print("Completed")
 
